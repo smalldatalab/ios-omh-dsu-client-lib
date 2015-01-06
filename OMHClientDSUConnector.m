52ee8b23476af95f055bd3eb639ffa43f7cc067c
Charlie Forkish <cforkish@gmail.com>
2015-01-07 00:28:22 +0700

    added user email property.

--- OMHClientDSUConnector.m
+++ OMHClientDSUConnector.m
@@ -22,6 +22,7 @@ NSString * const kDSUBaseURL = @"https://lifestreams.smalldata.io/dsu/";
 @property (nonatomic, strong) GPPSignIn *gppSignIn;
 @property (nonatomic, strong) AFHTTPSessionManager *httpSessionManager;
 
+@property (nonatomic, strong) NSString *signedInUserEmail;
 @property (nonatomic, strong) NSString *dsuAccessToken;
 @property (nonatomic, strong) NSString *dsuRefreshToken;
 @property (nonatomic, strong) NSDate *accessTokenDate;
@@ -257,6 +258,11 @@ NSString * const kDSUBaseURL = @"https://lifestreams.smalldata.io/dsu/";
 
 - (void)submitDataPoint:(NSDictionary *)dataPoint
 {
+    if (!self.isSignedIn) {
+        NSLog(@"attempting to submit data point while not signed in");
+        return;
+    }
+    
     [self.pendingDataPoints addObject:dataPoint];
     [self saveClientState];
     
@@ -342,12 +348,18 @@ NSString * const kDSUBaseURL = @"https://lifestreams.smalldata.io/dsu/";
     }
     else {
         NSString *serverCode = [GPPSignIn sharedInstance].homeServerAuthorizationCode;
-        NSLog(@"serverCode: %@", serverCode);
         if (serverCode != nil) {
+            NSLog(@"signed in user email: %@", auth.userEmail);
+            self.signedInUserEmail = auth.userEmail;
             [self signInToDSUWithServerCode:serverCode];
         }
         else {
             NSLog(@"failed to receive server code from google auth");
+            if (self.signInDelegate) {
+                NSError *serverCodeError = [NSError errorWithDomain:@"OMHClientError" code:0 userInfo:nil];
+                [self.signInDelegate OMHClientSignInFinishedWithError:serverCodeError];
+            }
+            
         }
     }
 }
@@ -393,11 +405,18 @@ NSString * const kDSUBaseURL = @"https://lifestreams.smalldata.io/dsu/";
 {
     NSLog(@"sign out");
     [self.gppSignIn signOut];
+    self.isAuthenticated = NO;
+    self.isAuthenticating = NO;
     
+    self.signedInUserEmail = nil;
     self.dsuAccessToken = nil;
     self.dsuRefreshToken = nil;
     self.accessTokenDate = nil;
     self.accessTokenValidDuration = 0;
+    
+    // TODO: once archived client is associated with a user we don't need to remove pending data points
+    [self.pendingDataPoints removeAllObjects];
+    
     [self saveClientState];
 }
 
